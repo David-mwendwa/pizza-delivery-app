f462021877e79e15f33d12ff1f3b360ae5861cc3
techdave <klatzdavie19@gmail.com>
2023-03-11 15:46:12 +0300

    add pizza description modal

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from 'react-bootstrap';
 import useInput from '../utils/useInput';
 
 const Pizza = ({ pizza }) => {
   const { values, handleChange } = useInput({ quantity: 1, variant: 'small' });
   const { quantity, variant } = values;
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div
       style={{ margin: '70px' }}
       className='shadow p-3 mb-5 bg-white rounded'>
-      <h1>{pizza.name}</h1>
-      <img
-        src={pizza.image}
-        className='img-fluid'
-        alt={pizza.name}
-        style={{ height: '200px', width: '200px' }}
-      />
+      <div onClick={handleShow}>
+        <h1>{pizza.name}</h1>
+        <img
+          src={pizza.image}
+          className='img-fluid'
+          alt={pizza.name}
+          style={{ height: '200px', width: '200px' }}
+        />
+      </div>
       <div className='flex-container'>
         <div className='m-1 w-100'>
           <p>Variants</p>
@@ -45,12 +53,36 @@ const Pizza = ({ pizza }) => {
 
       <div className='flex-container'>
         <div className='m-1 w-100'>
-          <h1 className='m-1'>Price: Ksh.{pizza.prices[0][variant] * quantity}</h1>
+          <h1 className='m-1'>
+            Price: Ksh.{pizza.prices[0][variant] * quantity}
+          </h1>
         </div>
         <div className='m-1 w-100'>
           <button className='btn'>ADD TO CART</button>
         </div>
       </div>
+
+      <Modal show={show}>
+        <Modal.Header>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <img
+            className='img-fluid'
+            src={pizza.image}
+            alt={pizza.name}
+            style={{ height: '400px' }}
+          />
+          <p>{pizza.description}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className='btn' onClick={handleClose}>
+            CLOSE
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
