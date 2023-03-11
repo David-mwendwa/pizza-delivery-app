da3f747ab6ce1676c4fafe5f805071c45ef50405
techdave <klatzdavie19@gmail.com>
2023-03-11 13:53:24 +0300

    create navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <a className='navbar-brand' href='/'>
+        PIZZA DELIVERY
+      </a>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarText'
+        aria-controls='navbarText'
+        aria-expanded='false'
+        aria-label='Toggle navigation'>
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarText'>
+        <ul className='navbar-nav ml-auto'>
+          <li className='nav-item'>
+            <a className='nav-link' href='/login'>
+              Login
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='/cart'>
+              Cart
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
