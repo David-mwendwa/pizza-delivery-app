33597e1419e75536f52f4d6eaebf97c3e3f71f4f
techdave <klatzdavie19@gmail.com>
2023-03-11 18:22:37 +0300

    add mongoDB connection

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.json({ success: true, page: 'Homepage' });
@@ -8,6 +10,12 @@ app.get('/', (req, res) => {
 app.use('*', (req, res) => {
   res.send('Route does not exist');
 });
+
+// mongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to mongoDB'))
+  .catch((err) => console.log(`Could not connect to the database`, err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
