5ea9980e26f56be12d0bb4b666511f53a7efac15
techdave <klatzdavie19@gmail.com>
2023-03-14 04:57:56 +0300

    improve user and pizza schema

--- models/pizzaModel.js
+++ models/pizzaModel.js
@@ -2,12 +2,35 @@ const mongoose = require('mongoose');
 
 const pizzaSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    variants: [],
-    prices: [],
-    category: { type: String, required: true },
-    image: { type: String, required: true },
-    description: { type: String, required: true },
+    name: {
+      type: String,
+      required: [true, 'Please enter a name'],
+      trim: true,
+    },
+    variants: [
+      {
+        type: String,
+      },
+    ],
+    prices: {
+      type: String,
+      required: [true, 'Please enter a price'],
+    },
+    category: {
+      type: String,
+      required: [true, 'Please select a category'],
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: [true, 'Please provide product image'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please enter  product description'],
+      trim: true,
+    },
   },
   {
     timestamps: true,
--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,39 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    name: {
+      type: String,
+      required: [true, 'Please enter your name'],
+      minlength: 3,
+      maxlength: [30, 'Your name cannot exceed 30 characters'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter your email'],
+      unique: true,
+      validate: [validator.isEmail, 'Please enter a valid email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter your password'],
+      minlength: [6, 'Your password must be longer than 6 characters'],
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+      trim: true,
+      validate: {
+        // This only works on .create() or .save()
+        validator: function (val) {
+          return this.password === val;
+        },
+        message: "Passwords don't match",
+      },
+    },
     isAdmin: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
